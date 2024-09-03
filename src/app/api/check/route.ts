05d29546238609937b46b0dce6bc849f052e37da
Davi Rodrigues <developer.davi@gmail.com>
2024-09-02 23:50:43 -0300

    fix: solve base url resolution issue

--- src/app/api/check/route.ts
+++ src/app/api/check/route.ts
@@ -4,7 +4,7 @@ import { getPostThread } from "@/app/bot/services/get-post-thread";
 import { updateSeen } from "@/app/bot/services/updateSeen";
 import { uploadBlob } from "@/app/bot/services/upload-blob";
 import { BASE_URL } from "@/app/constants";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 // disable static page generation
 export const revalidate = 0;
@@ -13,42 +13,46 @@ export const GET = async (_request: Request) => {
   const { seenAt, notifications } = await getNotifications();
 
   for (const notification of notifications.filter((n) => !n.isRead)) {
-    const thread = await getPostThread(notification.uri);
-    const post = thread.post as any;
-    const parentData = {
-      uri: post.uri,
-      cid: post.cid,
-    };
-    const { data: image } = await axios.get(`${BASE_URL}/api/print`, {
-      params: {
+    try {
+      const thread = await getPostThread(notification.uri);
+      const post = thread.post as any;
+      const parentData = {
         uri: post.uri,
-      },
-      responseType: "arraybuffer",
-      responseEncoding: "binary",
-    });
-    const blob = await uploadBlob(image);
-    await createRecord({
-      text: "Here is a screenshot of this post:",
-      createdAt: new Date().toISOString(),
-      embed: {
-        $type: "app.bsky.embed.images",
-        images: [
-          {
-            alt: "A screenshot from this post",
-            image: blob,
-          },
-        ],
-      },
-      reply: {
-        parent: parentData,
-        root: post.record.reply
-          ? {
-              uri: post.record.reply.root.uri,
-              cid: post.record.reply.root.cid,
-            }
-          : parentData,
-      },
-    });
+        cid: post.cid,
+      };
+      const { data: image } = await axios.get(`${BASE_URL}/api/print`, {
+        params: {
+          uri: post.uri,
+        },
+        responseType: "arraybuffer",
+        responseEncoding: "binary",
+      });
+      const blob = await uploadBlob(image);
+      await createRecord({
+        text: "Here is a screenshot of this post:",
+        createdAt: new Date().toISOString(),
+        embed: {
+          $type: "app.bsky.embed.images",
+          images: [
+            {
+              alt: "A screenshot from this post",
+              image: blob,
+            },
+          ],
+        },
+        reply: {
+          parent: parentData,
+          root: post.record.reply
+            ? {
+                uri: post.record.reply.root.uri,
+                cid: post.record.reply.root.cid,
+              }
+            : parentData,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   await updateSeen(
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,6 @@
-export const BASE_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+export const BASE_URL =
+  process.env.VERCEL_ENV === "production"
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
